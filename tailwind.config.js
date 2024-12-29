1c58595c37aadc4ea8920cf835aa7e71962c9e8c
bretek <josephmadden999@protonmail.com>
2024-12-29 00:59:47 +0000

    feature: created hugo theme

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./programmingBlog/themes/myTheme/content/**/*.{html,js}", "./programmingBlog/themes/myTheme/layouts/**/*.{html,js}", "./programmingBlog/assets/css/**/*.css"],
+  theme: {
+    extend: {
+      fontFamily : {
+        'primary' : 'font-sans'
+      },
+      colors: {
+        'carbon': {
+          400: '#7a7b7c',
+          700: '#262626',
+          800: '#161616',
+          900: '#0c0c0c',
+        },
+        'offwhite' : '#f2f4f8',
+        'primary' : {
+          500: '#78a9ff',
+          700: '#8977ff',
+        },
+        'secondary': {
+          500: '#c690ff',
+        },
+        'tertiary' : {
+          500: '#00c260',
+        }
+      },
+    },
+  },
+  plugins: [],
+}
+
